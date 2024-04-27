aaad62b552631906bcbf126cd199e628d2504c0e
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2024-04-27 15:01:29 +0530

    TEST

--- include/config.php
+++ include/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'sagartec_shuja');
-define('DB_PASS', '21);z%FKdcIq');
-define('DB_NAME', 'sagartec_sagartech');
+// define('DB_SERVER', 'localhost');
+// define('DB_USER', 'sagartec_shuja');
+// define('DB_PASS', '21);z%FKdcIq');
+// define('DB_NAME', 'sagartec_sagartech');
 
 // define('DB_SERVER', 'localhost');
 // define('DB_USER', 'root');
@@ -11,12 +11,13 @@ define('DB_NAME', 'sagartec_sagartech');
 // define('DB_NAME', 'sagartec_sagartech');
 
 define('WEB_URL', "https://sagartech.co.in/");
-error_reporting(error_reporting() & ~E_WARNING);
+// error_reporting(error_reporting() & ~E_WARNING);
 
 
-$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-// Check connection
-if (mysqli_connect_errno()) {
-	http_response_code(400);
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+// $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+// // Check connection
+// if (mysqli_connect_errno()) {
+// 	http_response_code(400);
+// 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// }
+?>
