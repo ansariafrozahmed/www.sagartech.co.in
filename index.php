4c9c9af2c8a2e86f7db61466efa86b8041f61b4a
Aafreen Sayyed <Ms.Sayyedaafreen786@gmail.com>
2025-02-13 18:10:44 +0530

    Home page og title, desc and image updated

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include ('include/config.php');
 <head>
 
     <head>
-        <title>Web Development and Digital Marketing in Mumbai - Sagar Tech</title>
+        <title>Web Development and Digital Marketing Agency in Mumbai - Sagar Tech</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
 
@@ -38,12 +38,12 @@ include ('include/config.php');
         <link rel="canonical" href="https://sagartech.co.in/">
         <meta property="og:locale" content="en_US">
         <meta property="og:type" content="website">
-        <meta property="og:title" content="Web Development and Digital Marketing in Mumbai - Sagar Tech">
+        <meta property="og:title" content="Web Development and Digital Marketing Agency in Mumbai - Sagar Tech">
         <meta property="og:description"
             content="Sagar Tech is one of the best web development and Digital Marketing company in Mumbai. We provide 360 degree marketing service. Grow your Business with Us.">
         <meta property="og:url" content="https://sagartech.co.in/">
         <meta property="og:site_name" content="Sagar Tech - Technical Solution">
-        <meta property="og:image" content="https://sagartech.co.in/images/bg/square_Logo_st.jpg">
+        <meta property="og:image" content="https://sagartech.co.in/images/sagartech1.png">
         <meta name="google-site-verification" content="EiGydxSCnJYDG7kwWzWMRW7-ciXvjatEdxGN_XGYtiY" />
         <meta name="p:domain_verify" content="d76965cda6bebd0c2cfde0c968d34c36" />
         <!-- Meta Pixel Code -->
