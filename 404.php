23399e2a8ea27eb4e1de58a1763369ac003a98eb
Aafreen Sayyed <Ms.Sayyedaafreen786@gmail.com>
2025-02-17 14:52:12 +0530

    404 page modified

--- 404.php
+++ 404.php
@@ -11,17 +11,17 @@ header("Access-Control-Allow-Origin: *");
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" href="images/logoonly.jpg" />
+    <link rel="shortcut icon" href="/images/logoonly.jpg" />
     <!-- bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <!-- main style -->
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="/css/style.css" rel="stylesheet" type="text/css" />
     <!-- responsive -->
-    <link href="css/responsive.css" rel="stylesheet" type="text/css" />
-    <link href="css/newcssresponsive.css" rel="stylesheet" type="text/css" />
+    <link href="/css/responsive.css" rel="stylesheet" type="text/css" />
+    <link href="/css/newcssresponsive.css" rel="stylesheet" type="text/css" />
     <!-- custom -->
-    <link href="css/custom.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <link href="/css/custom.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="/css/index.css" type="text/css">
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156939866-1"></script>
     <script>
@@ -62,25 +62,25 @@ header("Access-Control-Allow-Origin: *");
     <!-- loading -->
     <div id="loading">
         <div id="loading-center">
-            <img src="images/logoonly.jpg" alt="loder">
+            <img src="/images/logoonly.jpg" alt="loder">
         </div>
     </div>
     <!-- loading End -->
     <!-- HEADER  -->
     <?php
     $page = 'home';
-    include ('include/newHeader.php');
+    include('/include/newHeader.php');
     ?>
    
-    <img src="images/404.jpeg" width="100%" alt="">
+    <img src="/images/404.jpeg" width="100%" alt="">
     <div class="col-lg-12 text-center" style="margin-bottom: 50px;">
         <a class="404ckjd" href="https://sagartech.co.in/">Go Home</a>
     </div>
     <!-- /HEADER END -->
 
-    <?php include("include/footer.php"); ?>
-
     <!-- Footer -->
+    <?php include("/include/footer.php"); ?>
+
     <!-- back-to-top -->
     <div id="back-to-top">
         <a class="top" id="top" href="#top"> <i class="ion-ios-arrow-up"></i> </a>
@@ -88,33 +88,33 @@ header("Access-Control-Allow-Origin: *");
     <!-- back-to-top End -->
     <!--================ Jquery =================-->
     <!-- Jquery  -->
-    <script src="js/jquery.min.js"></script>
+    <script src="/js/jquery.min.js"></script>
     <!-- popper  -->
-    <script src="js/popper.min.js"></script>
+    <script src="/js/popper.min.js"></script>
     <!--  bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
     <!-- Google captcha code Js -->
     <script src='../../../../www.google.com/recaptcha/api.js'></script>
     <!-- Mega Menu -->
-    <script src="js/mega-menu/mega_menu.js"></script>
+    <script src="/js/mega-menu/mega_menu.js"></script>
     <!-- Main -->
-    <script src="js/main.js"></script>
+    <script src="/js/main.js"></script>
 
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <!-- price_range_script -->
-    <script src="js/price_range_script.js"></script>
+    <script src="/js/price_range_script.js"></script>
     <!-- modernizr.custom -->
-    <script src="js/modernizr.custom.js"></script>
+    <script src="/js/modernizr.custom.js"></script>
     <!-- jquerypp.custom -->
-    <script src="js/jquerypp.custom.js"></script>
+    <script src="/js/jquerypp.custom.js"></script>
     <!-- bookblock -->
-    <script src="js/jquery.bookblock.js"></script>
+    <script src="/js/jquery.bookblock.js"></script>
     <!-- style-customizer-->
-    <script src="js/style-customizer.js"></script>
+    <script src="/js/style-customizer.js"></script>
     <!-- Custom -->
-    <script src="js/custom.js"></script>
+    <script src="/js/custom.js"></script>
     <!-- WhatsHelp.io widget -->
-    <script src="js/notcopyable.js"></script>
+    <script src="/js/notcopyable.js"></script>
    
 
     <script type="text/javascript">
