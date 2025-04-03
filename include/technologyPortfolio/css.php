2d449d68495d1d91c788db117bdcf6317949347d
Danish <s.danish0827@gmail.com>
2025-04-03 17:34:26 +0530

    changes updated

---
+++ include/technologyPortfolio/css.php
@@ -0,0 +1,330 @@
+<?php
+// $slides = array(
+//   array(
+//     'type' => 'Ecommerce Website',
+//     'image' => './image/homepage/BSAuto.webp',
+//     'web_title' => 'BS Auto Accessories',
+//     'desc' => "When it comes to e-commerce website its user interface is a big challenge we have some best Wordpress Developers who can make your websites user friendly, easy to navigate product for customers with the help of latest technology no matter how many products are listed on your website it won't create a bad user experience."
+// ),
+//     array(
+//         'type' => 'Business Website',
+//         'image' => './image/homepage/MithiyajMockup.webp',
+//         'web_title' => 'Mithiyaj',
+//         'desc' => 'At Mithiyaj, we understand the universal craving for the finer things in life. With an unwavering commitment to quality, creativity, and affordability, we have curated a menu that spans a spectrum of delectable treats. From sumptuous cakes and pastries to timeless Indian sweets, delicate Baklavas, and luxurious gift hampers, we are here to transform your moments of indulgence into everlasting memories.'
+//     ),
+//     array(
+//         'type' => 'Catalogue Website',
+//         'image' => './image/homepage/ASAudio.webp',
+//         'web_title' => 'AS Audio',
+//         'desc' => 'Amaze Sound Audio Private Limited is a professional large-scale importer/manufacturer of Power Amplifiers, Professional Speakers, HF, Live Mixers, DJ Mixers, Microphones, Digital Processors, Manual Processors and Speaker Cabinets. AS Audio is driven by our Dedication to quality and most important Reliability.'
+//     ),
+//     array(
+//         'type' => 'Builders Website',
+//         'image' => './image/homepage/folksco.webp',
+//         'web_title' => 'Folksco',
+//         'desc' => 'At Folksco, our team works with you to create spaces for Best Interior Designer in Mumbai that are truly special. Our team is the pillars of our projects; they run our studio with passion, vision, and energy. As a team, we have made Eye for Detail, Value Engineering,'
+//     ),
+
+//     array(
+//         'type' => 'Logistics Website',
+//         'image' => './image/homepage/amarClearing.webp',
+//         'web_title' => 'Amar Clearing',
+//         'desc' => 'We are a Servicing Enterprise that handles varied activities in the sphere of Imports and Exports. Currently we are one of the leading Customs House Agents and Freight Forwarding Companies, rapidly moving towards the goal of emerging as one of the largest company dealing with Custom clearance, freight forwarding and logistics management.'
+//     ),
+//     array(
+//         'type' => 'Restaurant Website',
+//         'image' => './image/homepage/alnizami.webp',
+//         'web_title' => 'Al Nizami Darbar',
+//         'desc' => 'NIZAM, which in Arabic means an order or arrangement, was the title bestowed upon the ruler of Hyderabad. The NIZAMS of Hyderabad were of Turkish and Hadrahmi Arab descent. DARBAR, an audience hall, was the majestic court of a native prince that originated in Persia.'
+//     ),
+// );
+$slides = array(
+  array(
+    'type' => 'Human Resource Website',
+    'image' => './image/homepage/yovant-homepage.webp',
+    'web_title' => 'Yovant Recruitment',
+    'desc' => "Yovant Recruitment is a premier manpower and overseas recruitment agency specializing in sourcing skilled and unskilled workers for various industries across the Middle East. They have their own recruitment agencies in India, Nepal, Bangladesh, Pakistan, and Africa, ensuring a seamless hiring process. With a strong network and expertise, they cater to the staffing needs of construction, hospitality, healthcare, and other sectors. Their end-to-end recruitment solutions include screening, documentation, and deployment of workers. Yovant Recruitment is committed to providing reliable, efficient, and cost-effective hiring services for businesses worldwide."
+  ),
+  array(
+    'type' => 'Catalogue Website',
+    'image' => './image/homepage/thaiger-v1.webp',
+    'web_title' => 'Thaiger Nutraceuticals',
+    'desc' => "At Thaiger Nutraceuticals, we specialize in crafting premium health and wellness solutions tailored to support your lifestyle. Our website showcases a wide range of high-quality nutraceutical products designed with precision and care. Built with cutting-edge e-commerce technology, it ensures a seamless shopping experience, easy navigation, and secure transactions. Whether you're exploring our catalog or seeking personalized wellness support, we strive to provide an unparalleled user experience that reflects our commitment to quality and innovation."
+  ),
+  array(
+    'type' => 'Education Website',
+    'image' => './image/homepage/educ4te-v2.webp',
+    'web_title' => 'Educ4te',
+    'desc' => 'At Educ4te, we are dedicated to revolutionizing online learning by providing comprehensive, engaging, and interactive courses across a wide range of subjects. Our platform is designed to make education accessible and enjoyable for learners of all ages. With user-friendly navigation, cutting-edge technology, and a vast array of expertly crafted content, we empower individuals to achieve their educational goals and unlock their true potential. Join Educ4te and embark on a transformative learning journey tailored just for you!'
+  ),
+  array(
+    'type' => 'Business Website',
+    'image' => './image/homepage/verify8-v1.webp',
+    'web_title' => 'Verify8',
+    'desc' => 'Verify8 is your trusted partner in ensuring accuracy, security, and compliance through cutting-edge verification solutions. Our platform is designed to simplify and streamline verification processes across industries, offering fast, reliable, and efficient services tailored to your needs. With advanced technology and a user-focused interface, Verify8 empowers businesses and individuals to verify identities, documents, and data with confidence. Choose Verify8 for seamless verification that drives trust and success.'
+  ),
+  array(
+    'type' => 'Business Website',
+    'image' => './image/homepage/dontquit-v2.webp',
+    'web_title' => "Don't Quit Ventures",
+    'desc' => "Don't Quit Ventures is more than just a brand; it's a movement dedicated to fueling perseverance, passion, and progress. Our platform is designed to inspire and empower entrepreneurs, creators, and dreamers to push boundaries and achieve their goals. Through innovative solutions, expert guidance, and a vibrant community, we help you navigate challenges and turn ambitions into reality. At Don't Quit Ventures, we believe success starts with resilience—because quitting is never an option."
+  ),
+
+  array(
+    'type' => 'Travel Website',
+    'image' => './image/homepage/classic-v1.webp',
+    'web_title' => 'Go Classic Tour',
+    'desc' => "Go Classic Tour is your gateway to unforgettable travel experiences, offering expertly curated tours that combine luxury, adventure, and cultural immersion. Our user-friendly platform allows you to explore a wide range of destinations and customizable packages tailored to your preferences. Whether you're seeking a serene escape or an exhilarating journey, Go Classic Tour ensures every moment of your trip is seamless and memorable. Discover the world with us and create stories that last a lifetime!"
+  ),
+  array(
+    'type' => 'Business Website',
+    'image' => './image/homepage/alsalama-v2.webp',
+    'web_title' => 'Noor Al Salama',
+    'desc' => 'Al Salama Accident Repair Center is your trusted partner in restoring vehicles to their pristine condition. With state-of-the-art facilities and a team of highly skilled professionals, we specialize in providing top-quality accident repair services that prioritize safety and excellence. From minor scratches to major repairs, our commitment to precision, efficiency, and customer satisfaction ensures your vehicle gets the care it deserves. Trust Al Salama to get you back on the road with confidence.'
+  ),
+  // array(
+  //   'type' => 'Ecommerce Website',
+  //   'image' => './image/homepage/newqualityware-v1.webp',
+  //   'web_title' => 'New Quality Ware',
+  //   'desc' => "New Quality Ware is dedicated to delivering premium-quality kitchenware and home essentials designed to elevate your lifestyle. Our products combine innovation, durability, and style to meet the needs of modern living. Whether you're upgrading your kitchen tools or enhancing your home spaces, our thoughtfully curated collection ensures functionality and elegance in every detail. Explore New Quality Ware and discover the perfect balance of quality and practicality for your home."
+  // ),
+  array(
+    'type' => 'Ecommerce Website',
+    'image' => './image/homepage/trillionair-mockup.webp',
+    'web_title' => 'Trillionaire',
+    'desc' => "Trillionaire Auto Parts is committed to providing premium-quality automotive parts and accessories designed to enhance your vehicle’s performance and style. Our products blend innovation, durability, and precision to meet the demands of modern vehicles. Whether you're upgrading your car’s functionality or enhancing its aesthetics, our carefully curated selection ensures top-tier quality and reliability in every detail. Explore Trillionaire Auto Parts and experience the perfect combination of performance and excellence for your vehicle."
+  ),
+);
+?>
+<style>
+  .main-div-portfolio {
+    padding: 40px 0px 10px 0px;
+  }
+
+  swiper-container {
+    width: 100%;
+    height: 65vh;
+  }
+
+  swiper-slide {
+    padding: 10px 50px;
+    text-align: center;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .slide-inner-div {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    gap: 50px;
+  }
+
+  .inner-left {
+    width: 50%;
+    height: 100%;
+  }
+
+  .inner-left img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+    padding: 25px;
+  }
+
+  .inner-right {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    padding: 20px;
+    gap: 5px;
+  }
+
+  .inner-right .type {
+    font-size: 15px;
+    color: #FF0808;
+    font-weight: 500;
+
+  }
+
+  .inner-right .web_title {
+    font-size: 40px;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+
+  .inner-right .desc {
+    margin-top: 10px;
+    font-size: 15px;
+    line-height: normal;
+  }
+
+  .portfolio-heading {
+    font-size: 40px;
+    font-weight: 700;
+    /* padding:20px 0px; */
+    text-align: center;
+    color: #2b2a2a;
+    text-transform: uppercase;
+  }
+
+  @media only screen and (max-width: 800px) {
+    swiper-container {
+      width: 100%;
+      height: auto;
+      padding: 10px 0px;
+    }
+
+    swiper-slide {
+      padding: 20px;
+      text-align: center;
+      font-size: 18px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    swiper-slide img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .slide-inner-div {
+      height: 100%;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+    }
+
+    .inner-left {
+      width: 100%;
+      height: 100%;
+    }
+
+    .inner-left img {
+      height: 100%;
+      width: 100%;
+      object-fit: contain;
+    }
+
+    .inner-right {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      justify-content: center;
+      padding: 0px;
+      gap: 0px;
+    }
+
+    .inner-right .type {
+      font-size: 14px;
+      color: #ff0808;
+      font-weight: 500;
+
+    }
+
+    .inner-right .web_title {
+      font-size: 25px;
+      font-weight: 600;
+      text-align: left;
+    }
+
+    .inner-right .desc {
+      font-size: 14px;
+      line-height: normal;
+    }
+  }
+
+  /* CSS */
+  .button-62 {
+    background: linear-gradient(to bottom right, #FF0808, #fc6262);
+    border: 0;
+    border-radius: 12px;
+    text-align: center;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: inline-block;
+    font-family: -apple-system, system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 2.5;
+    outline: transparent;
+    padding: 5px 30px;
+    text-align: center;
+    text-decoration: none;
+    transition: box-shadow .2s ease-in-out;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    white-space: nowrap;
+  }
+
+  .button-62:not([disabled]):focus {
+    box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+  }
+
+  .button-62:not([disabled]):hover {
+    box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+    color: white;
+  }
+
+  .botttoonnn {
+    text-align: center;
+  }
+
+  .swiper-button-next svg,
+  .swiper-button-prev svg {
+    color: #FF0808 !important;
+  }
+
+  @media (max-width: 767px) {
+    .main-div-portfolio h2 {
+      font-size: 30px !important;
+
+    }
+  }
+</style>
+</head>
+
+<div class="main-div-portfolio">
+  <h2 class="portfolio-heading">Our <span style="color: #ff0808">Projects on CSS</span></h2>
+  <swiper-container class="mySwiper" navigation="true">
+    <?php foreach ($slides as $slide) : ?>
+      <swiper-slide>
+        <div class="slide-inner-div">
+          <div class="inner-left">
+            <img src="<?php echo $slide['image']; ?>" alt="<?php echo $slide['web_title']; ?>"
+              title="<?php echo $slide['web_title']; ?>">
+          </div>
+          <div class="inner-right">
+            <span class="type"><?php echo $slide['type']; ?></span>
+            <h5 class="web_title"><?php echo $slide['web_title']; ?></h5>
+            <p class="desc"><?php echo $slide['desc']; ?></p>
+          </div>
+        </div>
+      </swiper-slide>
+    <?php endforeach; ?>
+  </swiper-container>
+  <!-- <div class="botttoonnn">
+    <a href="portfolio/website" class="button-62" role="button">View All</a>
+  </div> -->
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
