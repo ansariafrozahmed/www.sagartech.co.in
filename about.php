5b6767adbf518e8fe5726b46a9e722c65d4def25
shariqueDev <shariquedev966@gmail.com>
2025-04-19 12:45:37 +0530

    enable loader

--- about.php
+++ about.php
@@ -462,11 +462,11 @@
 
 <body>
   <!-- loading -->
-  <!-- <div id="loading">
+  <div id="loading">
     <div id="loading-center">
       <img src="images/logoonly.jpg" alt="loder">
     </div>
-  </div> -->
+  </div>
 
   <?php
   $page = 'about';
