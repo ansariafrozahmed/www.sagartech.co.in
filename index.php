dfb2fa221f1f9edbd3e12bf5fd068f2141c32d4e
Aafreen Sayyed <Ms.Sayyedaafreen786@gmail.com>
2025-05-27 14:24:05 +0530

    Chat sticky icon code added in footer

--- index.php
+++ index.php
@@ -1346,30 +1346,6 @@ BS AUTO ACCESSORIES" loading="lazy">
     include("include/footer.php");
     ?>
     <script src="js/custom.js"></script>
-    <script type="text/javascript">
-        (function () {
-            var options = {
-                whatsapp: "+919820133303", // WhatsApp number
-                email: "info@sagartech.co.in", // Email
-                call_to_action: "Contact Us", // Call to action
-                button_color: "#FF0808", // Color of button
-                position: "left", // Position may be 'right' or 'left'
-                order: "whatsapp,email", // Order of buttons
-            };
-            var proto = document.location.protocol,
-                host = "getbutton.io",
-                url = proto + "//static." + host;
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = url + '/widget-send-button/js/init.js';
-            s.onload = function () {
-                WhWidgetSendButton.init(host, proto, options);
-            };
-            var x = document.getElementsByTagName('script')[0];
-            x.parentNode.insertBefore(s, x);
-        })();
-    </script>
 </body>
 
 </html>
