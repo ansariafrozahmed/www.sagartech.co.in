d582228a5c11c5a5956c160cf59015758121293f
Danish <s.danish0827@gmail.com>
2025-07-09 12:15:04 +0530

    oracle page added

--- include/newHeader.php
+++ include/newHeader.php
@@ -892,7 +892,7 @@ include('config.php');
                   </div>
                   <div class="titlee">
                     <img src="<?= WEB_URL ?>icons/oracle.svg" style="width: 30px;margin-top: -2px;" alt="" />
-                    <a href="<?= WEB_URL ?>mobile-app-development-company-in-mumbai">Oracle Service</a>
+                    <a href="<?= WEB_URL ?>oracle-service">Oracle Service</a>
                   </div>
                 </div>
               </div>
